b03adc3be2f8e51f0a3a12b8caa141355fd606cc
Stuart Hicks <stuarthicks@users.noreply.github.com>
2025-07-15 11:44:13 +0100

    strip unhelpful browser prefix

--- base64.go
+++ base64.go
@@ -3,6 +3,12 @@ package main
 import "encoding/base64"
 
 func tryBase64(i []byte) []byte {
+	// Some browsers add this prefix when you copy the response body from the devtools
+	const prefix = "data:application/octet-stream;base64,"
+	if len(i) >= len(prefix) && string(i[:len(prefix)]) == prefix {
+		i = i[len(prefix):]
+	}
+
 	o, err := base64.StdEncoding.DecodeString(string(i))
 	if err != nil {
 		return i
